cf5f3861469f8960e67bf79644815a873e56e7ca
alirezagh-1376 <ghadyani.alirezaa@gmail.com>
2026-05-02 10:26:49 +0330

    Footer.js

---
+++ src/Component/footer/Footer.js
@@ -0,0 +1,14 @@
+import'./Footer.css'
+function Footer(){
+    return(
+<footer>
+      <div class="container">
+        <p class="copyright">
+          © Copyright <strong>Iran</strong>. All Rights Reserved
+        </p>
+        <p>Designed by Iran</p>
+      </div>
+    </footer>
+    )
+}
+export default Footer;
