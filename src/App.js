9b20ad06da6bf2de6ce845344e5d2520fe4cb7f5
alirezagh-1376 <ghadyani.alirezaa@gmail.com>
2026-05-02 10:37:02 +0330

    App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import About from "./Component/about/about";
+import Header from "./Component/header/Header";
+import Service from "./Service/Service";
+import Blog from "./Component/blog/Blog";
+import Footer from "./Component/footer/Footer";
+
+function App (){
+    return(
+<>
+    <Header/>
+    <About/>
+    <Service/>
+    <Blog/>
+    <Footer/>
+</>
+    );
+}
+export default App
